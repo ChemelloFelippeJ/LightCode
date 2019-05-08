f2ab293f7964168b7e59923c20c1c550f2ad6e26
felippe <felippe.chemello@kinghost.com.br>
2019-05-08 12:31:51 -0300

    Inserindo index.html, bootstrap e ColorirFoto.php

---
+++ www/colorirFoto.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>LightCode</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <a class="navbar-brand" href="#">LightCode</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="colorirFoto.php">Colorir Foto</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<form method="post" action="#" enctype="multipart/form-data">
+    <label>Arquivo:</label>
+    <input type="file" name="arquivo" />
+    <input type="submit" value="Enviar" />
+</form>
+
+
+<?php
+if(count($_POST) > 0) {
+
+//UPLOAD IMAGEM
+// Pasta onde o arquivo vai ser salvo
+    $_UP['pasta'] = 'uploads/';
+
+// Tamanho máximo do arquivo (em Bytes)
+    $_UP['tamanho'] = 1024 * 1024 * 25; // 2Mb
+
+// Array com as extensões permitidas
+    $_UP['extensoes'] = array('jpg', 'png');
+
+// Array com os tipos de erros de upload do PHP
+    $_UP['erros'][0] = 'Não houve erro';
+    $_UP['erros'][1] = 'O arquivo no upload é maior do que o limite do PHP';
+    $_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
+    $_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
+    $_UP['erros'][4] = 'Não foi feito o upload do arquivo';
+
+// Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
+    if ($_FILES['arquivo']['error'] != 0) {
+        die("Não foi possível fazer o upload, erro:<br />" . $_UP['erros'][$_FILES['arquivo']['error']]);
+        exit; // Para a execução do script
+    }
+
+// Caso script chegue a esse ponto, não houve erro com o upload e o PHP pode continuar
+
+// Faz a verificação da extensão do arquivo
+    $extensao = strtolower(end(explode('.', $_FILES['arquivo']['name'])));
+    if (array_search($extensao, $_UP['extensoes']) === false) {
+        echo "Por favor, envie arquivos com as seguintes extensões: jpg, png";
+        exit;
+    }
+
+// Faz a verificação do tamanho do arquivo
+    if ($_UP['tamanho'] < $_FILES['arquivo']['size']) {
+        echo "O arquivo enviado é muito grande, envie arquivos de até 2Mb.";
+        exit;
+    }
+
+    $nome_final = $_FILES['arquivo']['name'];
+
+    if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $_UP['pasta'] . $nome_final)) {
+// Upload efetuado com sucesso, exibe uma mensagem e um link para o arquivo
+        echo "Upload efetuado com sucesso!";
+        $urlImage = $_UP['pasta'] . $nome_final;
+    } else {
+// Não foi possível fazer o upload, provavelmente a pasta está incorreta
+        echo "Não foi possível enviar o arquivo, tente novamente";
+        exit;
+    }
+
+//COLORIR IMAGEM
+    require_once 'vendor/autoload.php';
+
+    $input = json_decode('{
+        "image": ${urlImage}
+    }');
+var_dump($input);
+    die();
+
+}
+?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+
